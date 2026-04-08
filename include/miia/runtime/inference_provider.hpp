6ee81c15afa9a38b27f716bd01ea676ee6404149
Vinicius Guimarães Marques Mattos <vinicius@seu-email.com>
2026-04-08 15:44:37 -0300

    fix(Arquivos-inference_provider.cpp-e-inference_provider.hpp-modificados): Arquivos inference_provider.cpp e inference_provider.hpp modificados
    BREAKING CHANGE:

--- include/miia/runtime/inference_provider.hpp
+++ include/miia/runtime/inference_provider.hpp
@@ -2,24 +2,29 @@
 
 #include "miia/runtime/inference_orchestrator.hpp"
 #include <string>
+#include <map>
+#include <vector>
 
 namespace miia::runtime {
 
-// Classe responsável por ser a "porta de entrada" do sistema.
-// Atua como uma fachada (Facade), escondendo a complexidade interna
-// do Orchestrator e dos runtimes.
-class InferenceProvider {
-public:
-    // Método principal exposto ao usuário.
-    // Recebe o tipo do modelo (ex: "numerical", "symbolic", etc)
-    // e o input a ser processado.
-    std::string infer(const std::string& type,
-                      const std::string& input);
-
-private:
-    // Orchestrator responsável por decidir qual runtime usar
-    // e como executar (concorrência, estado, etc).
-    InferenceOrchestrator orchestrator_;
+struct PredictionResult {
+    std::unordered_map<std::string, std::vector<float>> outputs;
+    std::string error_message;
 };
 
-}
+struct PredictionRequest {
+    std::string type; //numerical (rede neural)
+    std::string model_id; //ppooptimizationflight1
+    std::unordered_map<std::string, std::vector<float>> inputs;
+};
+
+class InferenceProvider {
+public:
+    InferenceProvider();
+    ~InferenceProvider();
+
+    PredictionResult predict(const PredictionRequest& request);
+
+private:
+    InferenceOrchestrator orchestrator_;
+};
